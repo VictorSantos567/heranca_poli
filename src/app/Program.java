fd591750479d47547533bf44c34b94058bb6d47a
VictorSantos567 <victoraugustos49@gmail.com>
2021-01-10 22:37:29 -0300

    Implementação das entidades

--- src/app/Program.java
+++ src/app/Program.java
@@ -8,6 +8,9 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		
+		
 		System.out.println("Hello Word");
 		sc.close();
 	}
