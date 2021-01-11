85caa9a2088d648107c107fb2483bfc191ea2f01
VictorSantos567 <victoraugustos49@gmail.com>
2021-01-10 21:09:15 -0300

    Add bibliotecas no program

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,9 +1,15 @@
 package app;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Hello Word");
+		sc.close();
 	}
 
 }
